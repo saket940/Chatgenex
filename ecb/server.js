a450f3e2ed4597443b3b3d07c77e0a30b386e657
saket940 <asdadarya2222@gmail.com>
2025-07-20 20:57:17 +0200

    Update server.js

--- ecb/server.js
+++ ecb/server.js
@@ -369,4 +369,4 @@ app.post("/api/chatbotg", async(req, res) => {
 });
 // Start Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => .log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
