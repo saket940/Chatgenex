7b1c874eaeb5f789cfe220b58883cfbe888608cc
saket940 <asdadarya2222@gmail.com>
2025-07-21 05:21:55 +0200

    Update server.js

--- ecb/server.js
+++ ecb/server.js
@@ -298,6 +298,7 @@ app.post("/api/chatbot", async (req, res) => {
     contents: `system :You are a helpful assistant. Use the provided context to answer briefly. If question is related to the context and the answer is not in the context, respond with: 'Sorry, I don’t have enough information to answer that .Here is some reference context data: ${trainingData},Here is some reference context from a PDF:\n${trainingDatapdf}.User:${userMessage}`
   })
     res.json({ botResponse:response.text || "No response & URL error" });
+    console.error(`system :You are a helpful assistant. Use the provided context to answer briefly. If question is related to the context and the answer is not in the context, respond with: 'Sorry, I don’t have enough information to answer that .Here is some reference context data: ${trainingData},Here is some reference context from a PDF:\n${trainingDatapdf}.User:${userMessage}`)
   } catch (error) {
     console.error("Error fetching response:", error);
     res.status(500).json({ error: "Internal Server Error & URL error" });
